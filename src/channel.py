fcc0550cd7870f1011fceeb65a03eec1a5d0c92b
Filin Mikhail <filin.md9@gmail.com>
2023-06-06 22:39:36 +0300

    change Channel constructor, and add methods get_service(), to_json()

--- src/channel.py
+++ src/channel.py
@@ -4,23 +4,42 @@ import os
 
 from googleapiclient.discovery import build
 
-# получение YT_API_KEY из переменных окружения с помощью os
-api_key: str = os.getenv('YT_API_KEY')
-
-# создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey=api_key)
-
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    # получение YT_API_KEY из переменных окружения с помощью os
+    api_key: str = os.getenv('YT_API_KEY')
+
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscribers = self.channel["items"][0]['statistics']['subscriberCount']
+        self.video_count = self.channel["items"][0]['statistics']['videoCount']
+        self.views = self.channel["items"][0]['statistics']['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        dict_to_print = json.dumps(channel, indent=2, ensure_ascii=False)
+        dict_to_print = json.dumps(self.channel, indent=2, ensure_ascii=False)
         print(dict_to_print)
+
+    def to_json(self, file_name):
+        with open(file_name, "w") as file:
+            json.dump(self.channel, file)
 
