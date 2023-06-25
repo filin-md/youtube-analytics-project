18b4ae924ef01dd173a68688364fd3614cd573d9
filin-md <filin.md9@gmail.com>
2023-06-25 11:04:32 +0300

    add try/except/finally blocks

--- src/video.py
+++ src/video.py
@@ -3,14 +3,22 @@ from src.channel import youtube_obj
 
 class Video:
     def __init__(self, video_id):
+        video_response = youtube_obj.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id).execute()
 
-        video_response = youtube_obj.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+        try:
+            self.title = video_response['items'][0]['snippet']['title']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.url = f'https://youtu.be/{self.video_id}'
+        except:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.url = None
+        finally:
+            self.video_id = video_id
 
-        self.video_id = video_id
-        self.title = video_response['items'][0]['snippet']['title']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
-        self.url = f'https://youtu.be/{self.video_id}'
 
     def __str__(self):
         return self.title
